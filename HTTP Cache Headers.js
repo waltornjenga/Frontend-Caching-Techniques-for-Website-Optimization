6db60a9d5f356275ed8241dc079204923245911e
waltornjenga <waiyakiwaltor@gmail.com>
2025-11-10 13:04:26 +0300

    Add static cache configuration

--- HTTP Cache Headers.js
+++ HTTP Cache Headers.js
@@ -1,9 +1,15 @@
-
+// cache-headers.js
 import crypto from 'crypto';
 
 class CacheHeaderManager {
   constructor() {
     this.etagCache = new Map();
-    this.config = {};
+    this.config = {
+      static: {
+        maxAge: 31536000, // 1 year
+        immutable: true,
+        vary: 'Accept-Encoding'
+      }
+    };
   }
 }
