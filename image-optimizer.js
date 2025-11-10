905d25d03ed68728e69ae1759b4071df6a82fe76
waltornjenga <waiyakiwaltor@gmail.com>
2025-11-10 13:57:44 +0300

    Implement responsive image set creation with multiple sizes and formats

--- image-optimizer.js
+++ image-optimizer.js
@@ -19,7 +19,43 @@ class IntelligentImageCache {
       return cached;
     }
 
-    return src;
+    const imageSet = await this.createResponsiveSet(src, options);
+    return imageSet[0].url;
+  }
+
+  async createResponsiveSet(src, options) {
+    const sizes = options.sizes || [400, 800, 1200, 1920];
+    const formats = options.formats || this.config.formats;
+    
+    const set = [];
+    
+    for (const size of sizes) {
+      for (const format of formats) {
+        const optimizedUrl = await this.generateOptimizedUrl(src, {
+          width: size,
+          format,
+          quality: this.config.quality
+        });
+        
+        set.push({
+          url: optimizedUrl,
+          width: size,
+          format
+        });
+      }
+    }
+    
+    return set.sort((a, b) => a.width - b.width);
+  }
+
+  async generateOptimizedUrl(src, options) {
+    const params = new URLSearchParams();
+    
+    if (options.width) params.append('w', options.width);
+    if (options.quality) params.append('q', options.quality);
+    if (options.format) params.append('fm', options.format);
+    
+    return `${src}?${params.toString()}`;
   }
 
   generateImageKey(src, options) {
